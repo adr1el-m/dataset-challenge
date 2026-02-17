3cf966e661225a0930e6ae769b7ffc58e05d4779
adriel <dagsmagalona@gmail.com>
2026-02-17 20:53:43 +0800

    fix: remove output export - let Vercel use native Next.js adapter

--- blast-slam-6-website/next.config.mjs
+++ blast-slam-6-website/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
   transpilePackages: ["three", "react-plotly.js", "plotly.js"],
   images: {
